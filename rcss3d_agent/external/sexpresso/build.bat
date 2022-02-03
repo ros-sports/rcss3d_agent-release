68919938cf65ab203c5941a55b89494428640a67
ijnek <kenjibrameld@gmail.com>
2022-02-04 07:09:12 +0900

    Trimmed the branch to only the rcss3d_agent_msgs sub directory

--- rcss3d_agent/external/sexpresso/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call cl /O2 /c sexpresso\sexpresso.cpp
-call lib sexpresso.obj /OUT:sexpresso.lib
-call cl /Isexpresso /O2 /c sexpresso_std\sexpresso_std.cpp
-call lib sexpresso_std.obj /OUT:sexpresso_std.lib
-
